6367398ac550c9827601cbdcdb0c274243cb535e
udk <udk1@bk.ru>
2019-06-15 15:43:16 +0300

    test

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using hackathon.Models;
+
+namespace hackathon.Controllers
+{
+    [Route("api/")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpGet]
+        public IActionResult Contact(string id)
+        {
+            Sicknens sicknens = new Sicknens() { response =id};
+            return Json(sicknens);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
