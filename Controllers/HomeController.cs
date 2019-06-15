d366d4ad5949a2191d343ce11dd03d754296906a
udk <udk1@bk.ru>
2019-06-15 19:06:45 +0300

    test sql v0.259

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,22 +22,23 @@ namespace hackathon.Controllers
         {
             Sicknens result = new Sicknens();
             var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-            try
-            {
-                var conn = new NpgsqlConnection(databaseUrl);
-                conn.Open();
-                NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM patient", conn);
-                var dr = command.ExecuteReader();
-                dr.Read();
-                result.id = (uint)dr[0];
-                result.name = (string)dr[1];
-                result.surname = (string)dr[0];
-                return Json(result);
-            }
-            catch(Exception e)
-            {
-                return Json(e);
-            }
+            //try
+            //{
+            //    var conn = new NpgsqlConnection(databaseUrl);
+            //    conn.Open();
+            //    NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM patient", conn);
+            //    var dr = command.ExecuteReader();
+            //    dr.Read();
+            //    result.id = (uint)dr[0];
+            //    result.name = (string)dr[1];
+            //    result.surname = (string)dr[0];
+            //    return Json(result);
+            //}
+            //catch(Exception e)
+            //{
+            //    return Json(e);
+            //}
+            return Json(databaseUrl);
         }
 
         public IActionResult Privacy()
